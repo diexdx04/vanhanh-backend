4144c8b6f2a8a944a95416b532cb5cf64ac5dc57
diexdx04 <diepdx.dev@gmail.com>
2024-11-14 11:06:45 +0700

    signin

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,18 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prima: PrismaService) {}
+  constructor(
+    private readonly prima: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async signup(name: string, email: string, password: string) {
     try {
@@ -27,5 +35,26 @@ export class UserService {
     } catch (error) {
       throw error;
     }
+  }
+
+  async signin(email: string, password: string) {
+    const user = await this.prima.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Email khong ton tai!');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Mat khau khong chinh xac!');
+    }
+
+    const payload = { email: user.email, id: user.id };
+    const token = this.jwtService.sign(payload);
+
+    return { access_token: token };
   }
 }
