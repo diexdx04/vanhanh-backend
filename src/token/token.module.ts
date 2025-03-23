440b94348a39e48e131bb28e1cc16a9d9b66f7fc
diexdx04 <diepdx.dev@gmail.com>
2025-03-23 14:54:34 +0700

    configModule.forRoot

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [TokenService],
   imports: [
+    ConfigModule.forRoot(), // Tải các biến môi trường từ tệp .env
+
     PrismaModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
